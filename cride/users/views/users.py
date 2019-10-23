4f00e46f396ee8a13504f001e2d1dd34d4ae97c8
Luis Gonzalez <luis.gonzalez@orbitec.pe>
2019-10-23 16:36:27 -0500

    % Actualizar modelo de circle clase 29

--- cride/users/views/users.py
+++ cride/users/views/users.py
@@ -7,8 +7,10 @@ from rest_framework.views import APIView
 
 # Serializers
 from cride.users.serializers import (
+    AccountVerificationSerializer,
     UserLoginSerializer,
-    UserModelSerializer
+    UserModelSerializer,
+    UserSignUpSerializer
 )
 
 
@@ -25,3 +27,27 @@ class UserLoginAPIView(APIView):
             'access_token': token
         }
         return Response(data, status=status.HTTP_201_CREATED)
+
+
+class UserSignUpAPIView(APIView):
+    """User login API view."""
+
+    def post(self, request, *args, **kwargs):
+        """Handle HTTP POST request."""
+        serializer = UserSignUpSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        data = UserModelSerializer(user).data
+        return Response(data, status=status.HTTP_201_CREATED)
+
+
+class AccountVerificationAPIView(APIView):
+    """Acoount Verifitation API view."""
+
+    def post(self, request, *args, **kwargs):
+        """Handle HTTP POST request."""
+        serializer = AccountVerificationSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        data = {'message': 'Congratulation, now go share some rides!'}
+        return Response(data, status=status.HTTP_200_OK)
